25db417d6b50f27c4891321b038adc3c6110757c
DavidHesGit <ch.davidhe@gmail.com>
2023-07-18 11:34:51 +0200

    deleted everything to try another approach

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,5 @@
 import javax.swing.*;
 
 public class Game {
-    public static void main(String[] args) {
-        JFrame game = new JFrame();
-        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        game.setResizable(false);
-        game.setTitle("Classic Chess");
 
-        Chessboard board = new Chessboard();
-        game.add(board);
-        game.pack(); // Reduce window size to the prefered size configured in the Chessboard class
-
-        // Puts it in the center
-        game.setLocationRelativeTo(null);
-        game.setVisible(true);
-
-        board.start();
-    }
 }
